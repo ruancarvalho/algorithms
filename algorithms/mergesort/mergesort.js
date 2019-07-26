7714da75f6816375c7fd0e8fb2abcc7d18e4d3c2
Ruan Carvalho <ruancarvalho@gmail.com>
2019-07-26 13:53:42 -0400

    feat(mergesort): add merge sort algorithm implementation

--- algorithms/mergesort/mergesort.js
+++ algorithms/mergesort/mergesort.js
@@ -1,7 +1,48 @@
-const mergesort = function(arrayA, arrayB) {
-  let merged = [];
+/**
+ * 
+ * @param {Array} items 
+ */
+const mergesort = function(items = []) {
+  if (items.length < 2) {    
+    return items
+  } else {
+    console.log('mergesort', items);
 
-  
+    let leftItems = items.slice(0, items.length / 2);
+    let rightItems = items.slice(items.length / 2);
+    
+    // divide all items
+    let left = mergesort(leftItems);
+    let right = mergesort(rightItems);
 
-  return merged;
+    // conquer: merge items into one array
+    return merge(left, right);
+  }
+}
+
+/**
+ * 
+ * @param {Array} left 
+ * @param {Array} right 
+ */
+const merge = function(left = [], right = []) {
+  let sorted = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      sorted.push(left.shift());
+    } else {
+      sorted.push(right.shift());
+    }
+  }
+
+  while (left.length) {
+    sorted.push(left.shift());
+  }
+
+  while (right.length){
+    sorted.push(right.shift());
+  }
+  console.log('sorted', sorted)
+  return sorted;
 }
